e36c3725e1453351b8cb42000405c26f5a461333
Dmitriy Myroshnyk <dmiroshnik@teambase.com>
2023-03-26 15:12:38 +0300

    3d commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var app = express();
-app.get("/url", (req, res, next) => {
+app.get("/url", function(req, res, next)  {
     res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
-app.listen(80, () => {
+app.listen(80, function() {
     console.log("Server running on port 80");
 });
